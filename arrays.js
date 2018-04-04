58baad45a28e25816ff27596ba3de2a2f6a855cd
AJ Speller <ajspeller@gmail.com>
2018-04-04 00:49:07 -0400

    Practing the Reduce method

--- arrays.js
+++ arrays.js
@@ -504,3 +504,54 @@ console.table(
     'Instructor'
   )
 );
+console.log(divider);
+
+/*
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+
+Examples:
+    
+    function isEven(val){
+        return val % 2 === 0;
+    }
+    
+    var arr = [1,2,3,4,5,6,7,8];
+    
+    partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
+    
+    function isLongerThanThreeCharacters(val){
+        return val.length > 3;
+    }
+    
+    var names = ['Elie', 'Colt', 'Tim', 'Matt'];
+    
+    partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
+*/
+
+var partition = (arr, callback) => {
+  return arr.reduce(
+    (acc, nextItem, idx, arr) => {
+      if (callback(nextItem)) {
+        acc[0].push(nextItem);
+      } else {
+        acc[1].push(nextItem);
+      }
+      return acc;
+    },
+    [[], []]
+  );
+};
+
+function isEven1(val) {
+  return val % 2 === 0;
+}
+
+console.table(partition([1, 2, 3, 4, 5, 6, 7, 8], isEven1)); // [[2,4,6,8], [1,3,5,7]];
+
+function isLongerThanThreeCharacters(val) {
+  return val.length > 3;
+}
+
+console.table(
+  partition(['Elie', 'Colt', 'Tim', 'Matt'], isLongerThanThreeCharacters)
+); // [['Elie', 'Colt', 'Matt'], ['Tim']]
